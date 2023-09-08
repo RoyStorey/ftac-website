1fc8f293ed71aac1ff3bafa726103aa7223a2507
Roy Storey <imlejiii@gmail.com>
2023-09-08 13:03:08 +0000

    media query header and footer

--- src/components/LearnMore.jsx
+++ src/components/LearnMore.jsx
@@ -2,7 +2,7 @@ import '../css/learn-more.css'
 
 function LearnMore(props) {
   return (
-    <div className="learn-more-container">
+    <div className="learn-more-container desktop-only">
       <h3>{props.ftacEvent.name}</h3>
       <p>{props.ftacEvent.description}</p>
       <a href='/contact'>Contact to Learn More</a>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,10 @@
 import '../css/header.css'
+import '../css/app.css'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import AirForce from '../media/aflogo.png'
 import React from 'react'
 import { useNavigate } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Header(props) {
   const navigate = useNavigate();
@@ -17,12 +20,13 @@ export default function Header(props) {
         <img className="header-image" draggable='false' src={AirForce} alt="air force logo" />
       </span>
       <h2 className='header-title'>{props.title}</h2>
-      <div className='header-links'>
+      <div className='header-links desktop-only'>
         <a className='header-link' id='' onClick={e => HandleClick(e)}>Home</a>
         <a className='header-link' id='about' onClick={e => HandleClick(e)}>About us</a>
         <a className='header-link' id='resources' onClick={e => HandleClick(e)}>Resources</a>
         <a className='header-link' id='contact' onClick={e => HandleClick(e)}>Contact us</a>
       </div>
+      <FontAwesomeIcon icon={faBars} className='bars-icon mobile-only'/>
     </div>
   )
 }
